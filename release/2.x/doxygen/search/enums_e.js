ac6055b0d1d5ceff68e2b492c21308925560a93b
Azure Pipelines <azuredevops@microsoft.com>
2022-06-15 17:15:05 +0000

    [ci skip] commit generated documentation

---
+++ release/2.x/doxygen/search/enums_e.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['securitydomain_0',['SecurityDomain',['../namespacekv.html#aa495b22a222edb1e7dc37493775eb1ed',1,'kv']]],
+  ['servicestatus_1',['ServiceStatus',['../namespaceccf.html#a113ec5aaf42643e9eedb09c929a534b9',1,'ccf']]],
+  ['sessionstate_2',['SessionState',['../classccf_1_1SplitIdentityResharingTracker.html#ad43a1de4ccff2a1a14b4311884ed07cb',1,'ccf::SplitIdentityResharingTracker']]],
+  ['signer_3',['Signer',['../classcrypto_1_1KeyPair.html#aa7f16ca77e1668f87151597888639dc4',1,'crypto::KeyPair']]],
+  ['starttype_4',['StartType',['../enclave__interface__types_8h.html#a74ebf9e22784b3dfab1360b7abd60bb4',1,'enclave_interface_types.h']]],
+  ['state_5',['State',['../namespacehttp.html#aefd1b5f9599515e7fff79656c880a107',1,'http']]],
+  ['status_6',['Status',['../classccf_1_1TLSEndpoint.html#a30e126f4091091b65a78c424aef7cded',1,'ccf::TLSEndpoint']]]
+];
---
+++ release/2.x/doxygen/search/enumvalues_2.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['candidate_0',['Candidate',['../namespacekv.html#a7f64527c99fd8ad1de3af022a15661aba25befec7033aa9feec4a5a586d4e1366',1,'kv']]],
+  ['cft_1',['CFT',['../consensus__type_8h.html#abe16e454ed4570b49c57c76d7fea083bad7c77838ce82c02672d0803ddc4fc2f7',1,'consensus_type.h']]],
+  ['channel_5fmsg_2',['channel_msg',['../namespaceccf.html#aea3c30cd6e176b6c348f884f0e3f22b5a001565c2cfea425953f7c2ac88d38cb2',1,'ccf']]],
+  ['closed_3',['closed',['../classccf_1_1TLSEndpoint.html#a30e126f4091091b65a78c424aef7cdeda473a85550fe9fcb14ab27ebaa2473541',1,'ccf::TLSEndpoint']]],
+  ['committed_4',['Committed',['../namespaceccf.html#aa8e2135f8c64ce434fc5b461e994ff1aafd8f656590df4b2d2b217b060b7204d3',1,'ccf::Committed()'],['../namespacekv.html#aa01efef390b9a3234d4f162c2614f979afd8f656590df4b2d2b217b060b7204d3',1,'kv::Committed()']]],
+  ['compact_5',['COMPACT',['../namespaceccf.html#ab051753e2e980361ab252ae1b7c0975ba2fd327c16dce4daeb885ef1a5c13c781',1,'ccf']]],
+  ['completed_6',['Completed',['../namespacekv.html#aa01efef390b9a3234d4f162c2614f979a07ca5050e697392c9ed47e6453f1453f',1,'kv']]],
+  ['consensus_5fmsg_7',['consensus_msg',['../namespaceccf.html#aea3c30cd6e176b6c348f884f0e3f22b5a948590a99cb280e32a158190538ca373',1,'ccf']]],
+  ['consensusnotallowed_8',['ConsensusNotAllowed',['../enclave__interface__types_8h.html#a00a4800613d7982c7e3ff63bdc7c6f27a97ba786dac713560a6b8699a2fb7f87d',1,'enclave_interface_types.h']]],
+  ['continue_9',['Continue',['../structccf_1_1historical_1_1StateCacheImpl_1_1Request.html#abac568a7c42bbbc6160ded02a9468a55aa0bfb8e59e6c13fc8d990781f77694fe',1,'ccf::historical::StateCacheImpl::Request']]],
+  ['corrupt_10',['Corrupt',['../structccf_1_1indexing_1_1FetchResult.html#a235623743aa656ac589c87ab9adaed02a76880bbac5714eb53a303138148342ca',1,'ccf::indexing::FetchResult']]]
+];
